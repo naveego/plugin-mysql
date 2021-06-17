c9a30f51338225f6ff432e3e279f28dd367fdd63
Wyatt Roehler <Wyatt.Roehler@aunalytics.com>
2021-06-17 12:16:59 -0400

    fix(replication): updated upsert record async

--- PluginMySQL/API/Replication/UpsertRecordAsync.cs
+++ PluginMySQL/API/Replication/UpsertRecordAsync.cs
@@ -22,92 +22,99 @@ namespace PluginMySQL.API.Replication
             {
                 await conn.OpenAsync();
                 
-                // try to insert
-                var querySb =
-                    new StringBuilder(
-                        $"INSERT INTO {Utility.Utility.GetSafeName(table.SchemaName, '`')}.{Utility.Utility.GetSafeName(table.TableName, '`')}(");
-                foreach (var column in table.Columns)
+                var primaryKey = table.Columns.Find(c => c.PrimaryKey);
+                var primaryValue = recordMap[primaryKey.ColumnName];
+                if (primaryKey.Serialize)
                 {
-                    querySb.Append($"{Utility.Utility.GetSafeName(column.ColumnName, '`')},");
+                    primaryValue = JsonConvert.SerializeObject(primaryValue);
                 }
 
-                querySb.Length--;
-                querySb.Append(") VALUES (");
-
-                foreach (var column in table.Columns)
+                if (!await RecordExistsAsync(connFactory, table, primaryValue.ToString()))
                 {
-                    if (recordMap.ContainsKey(column.ColumnName))
-                    {
-                        var rawValue = recordMap[column.ColumnName];
-                        if (column.Serialize)
-                        {
-                            rawValue = JsonConvert.SerializeObject(rawValue);
-                        }
-
-                        switch (column.DataType)
-                        {
-                            case "date":
-                                if (DateTime.TryParse(rawValue.ToString(), out var date))
-                                {
-                                    rawValue = date.ToString("yyyy-MM-dd");
-                                }
-                                else
-                                {
-                                    rawValue = null;
-                                }
-
-                                break;
-                            case "datetime":
-                                if (DateTime.TryParse(rawValue.ToString(), out var datetime))
-                                {
-                                    rawValue = datetime.ToString("yyyy-MM-dd HH:mm:ss");
-                                }
-                                else
-                                {
-                                    rawValue = null;
-                                }
-                                break;
-                            case "time":
-                                if (TimeSpan.TryParse(rawValue.ToString(), out var time))
-                                {
-                                    rawValue = time.ToString("c");
-                                }
-                                else
-                                {
-                                    rawValue = null;
-                                }
-                                break;
-                        }
-
-                        querySb.Append(rawValue != null
-                            ? $"'{Utility.Utility.GetSafeString(rawValue.ToString(), "'", "''")}',"
-                            : $"NULL,");
-                    }
-                    else
-                    {
-                        querySb.Append($"NULL,");
-                    }
-                }
-
-                querySb.Length--;
-                querySb.Append(");");
-
-                var query = querySb.ToString();
-
-                Logger.Debug($"Insert record query: {query}");
-
-                var cmd = connFactory.GetCommand(query, conn);
-
-                await cmd.ExecuteNonQueryAsync();
-            }
-            catch (Exception e)
-            {
-                try
-                {
-                    // update if it failed
+                    // insert record
                     var querySb =
                         new StringBuilder(
-                            $"UPDATE {Utility.Utility.GetSafeName(table.SchemaName, '`')}.{Utility.Utility.GetSafeName(table.TableName, '`')} SET ");
+                            $"INSERT INTO {Utility.Utility.GetSafeName(table.SchemaName)}.{Utility.Utility.GetSafeName(table.TableName)}(");
+                    foreach (var column in table.Columns)
+                    {
+                        querySb.Append($"{Utility.Utility.GetSafeName(column.ColumnName)},");
+                    }
+
+                    querySb.Length--;
+                    querySb.Append(") VALUES (");
+
+                    foreach (var column in table.Columns)
+                    {
+                        if (recordMap.ContainsKey(column.ColumnName))
+                        {
+                            var rawValue = recordMap[column.ColumnName];
+                            if (column.Serialize)
+                            {
+                                rawValue = JsonConvert.SerializeObject(rawValue);
+                            }
+                            
+                            switch (column.DataType)
+                            {
+                                case "date":
+                                    if (DateTime.TryParse(rawValue.ToString(), out var date))
+                                    {
+                                        rawValue = date.ToString("yyyy-MM-dd");
+                                    }
+                                    else
+                                    {
+                                        rawValue = null;
+                                    }
+
+                                    break;
+                                case "datetime":
+                                    if (DateTime.TryParse(rawValue.ToString(), out var datetime))
+                                    {
+                                        rawValue = datetime.ToString("yyyy-MM-dd HH:mm:ss");
+                                    }
+                                    else
+                                    {
+                                        rawValue = null;
+                                    }
+                                    break;
+                                case "time":
+                                    if (TimeSpan.TryParse(rawValue.ToString(), out var time))
+                                    {
+                                        rawValue = time.ToString("c");
+                                    }
+                                    else
+                                    {
+                                        rawValue = null;
+                                    }
+                                    break;
+                            }
+
+                            querySb.Append(rawValue != null
+                                ? $"'{Utility.Utility.GetSafeString(rawValue.ToString(), "'", "''")}',"
+                                : $"NULL,");
+                        }
+                        else
+                        {
+                            querySb.Append($"NULL,");
+                        }
+                    }
+
+                    querySb.Length--;
+                    querySb.Append(");");
+
+                    var query = querySb.ToString();
+
+                    Logger.Debug($"Insert record query: {query}");
+
+                    var cmd = connFactory.GetCommand(query, conn);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+                else
+                {
+                    // update record
+                    var querySb =
+                        new StringBuilder(
+                            $"UPDATE {Utility.Utility.GetSafeName(table.SchemaName)}.{Utility.Utility.GetSafeName(table.TableName)} SET ");
                     foreach (var column in table.Columns)
                     {
                         if (!column.PrimaryKey)
@@ -115,38 +122,60 @@ namespace PluginMySQL.API.Replication
                             if (recordMap.ContainsKey(column.ColumnName))
                             {
                                 var rawValue = recordMap[column.ColumnName];
-                                if (column.Serialize)
-                                {
-                                    rawValue = JsonConvert.SerializeObject(rawValue);
-                                }
+                            if (column.Serialize)
+                            {
+                                rawValue = JsonConvert.SerializeObject(rawValue);
+                            }
+                            
+                            switch (column.DataType)
+                            {
+                                case "date":
+                                    if (DateTime.TryParse(rawValue.ToString(), out var date))
+                                    {
+                                        rawValue = date.ToString("yyyy-MM-dd");
+                                    }
+                                    else
+                                    {
+                                        rawValue = null;
+                                    }
 
-                                if (rawValue != null)
-                                {
-                                    querySb.Append(
-                                        $"{Utility.Utility.GetSafeName(column.ColumnName, '`')}='{Utility.Utility.GetSafeString(rawValue.ToString(), "'", "''")}',");
-                                }
-                                else
-                                {
-                                    querySb.Append($"{Utility.Utility.GetSafeName(column.ColumnName, '`')}=NULL,");
-                                }
+                                    break;
+                                case "datetime":
+                                    if (DateTime.TryParse(rawValue.ToString(), out var datetime))
+                                    {
+                                        rawValue = datetime.ToString("yyyy-MM-dd HH:mm:ss");
+                                    }
+                                    else
+                                    {
+                                        rawValue = null;
+                                    }
+                                    break;
+                                case "time":
+                                    if (TimeSpan.TryParse(rawValue.ToString(), out var time))
+                                    {
+                                        rawValue = time.ToString("c");
+                                    }
+                                    else
+                                    {
+                                        rawValue = null;
+                                    }
+                                    break;
+                            }
+
+                            querySb.Append(rawValue != null
+                                ? $"{Utility.Utility.GetSafeName(column.ColumnName)}='{Utility.Utility.GetSafeString(rawValue.ToString(), "'", "''")}',"
+                                : $"{Utility.Utility.GetSafeName(column.ColumnName)}=NULL,");
                             }
                             else
                             {
-                                querySb.Append($"{Utility.Utility.GetSafeName(column.ColumnName, '`')}=NULL,");
+                                querySb.Append($"{Utility.Utility.GetSafeName(column.ColumnName)}=NULL,");
                             }
                         }
                     }
 
                     querySb.Length--;
 
-                    var primaryKey = table.Columns.Find(c => c.PrimaryKey);
-                    var primaryValue = recordMap[primaryKey.ColumnName];
-                    if (primaryKey.Serialize)
-                    {
-                        primaryValue = JsonConvert.SerializeObject(primaryValue);
-                    }
-
-                    querySb.Append($" WHERE {primaryKey.ColumnName} = '{primaryValue}'");
+                    querySb.Append($" WHERE {Utility.Utility.GetSafeName(primaryKey.ColumnName)} = '{primaryValue}'");
 
                     var query = querySb.ToString();
 
@@ -154,16 +183,11 @@ namespace PluginMySQL.API.Replication
 
                     await cmd.ExecuteNonQueryAsync();
                 }
-                catch (Exception exception)
-                {
-                    Logger.Error(e, $"Error Insert: {e.Message}");
-                    Logger.Error(exception, $"Error Update: {exception.Message}");
-                    throw;
-                }
-                finally
-                {
-                    await conn.CloseAsync();
-                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, $"Error Upsert Record: {e.Message}");
+                throw;
             }
             finally
             {
